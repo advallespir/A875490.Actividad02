b4cadeeb37d0ffa9b5a25647516336ce3ffa3b3d
ADVallespir <advallespir@gmail.com>
2021-04-10 00:10:01 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A875490.Actividad02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
