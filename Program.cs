ec4382e3cdb85b27e112788abbbd7ed42c1a44b6
ADVallespir <advallespir@gmail.com>
2021-04-12 19:28:43 -0300

    Consigna

--- Program.cs
+++ Program.cs
@@ -6,6 +6,20 @@ namespace A875490.Actividad02
     {
         static void Main(string[] args)
         {
+            /*
+             * 
+             *      -	Complete la codificación del ejercicio que se propone más abajo y compártala en un repositorio público de GitHub llamado “A[NroRegistro].Actividad02”
+                    -	Complete el formulario indicando número de registro y usuario (no del repositorio, que debe ser, en cualquier caso, el indicado arriba) en github: Vínculo al formulario
+                    -	Vínculo al formulario en texto plano: https://forms.office.com/Pages/ResponsePage.aspx?id=eY-BTISrUkWbfC_nFbDQ1TKuCrTwNtVHvHonU8CCmaBURjFVOUtYUDlENVJVSUNYSkhaQTJCUlZXMy4u
+
+                * -	Una empresa de cable necesita una aplicación para encolar las órdenes de instalación recibidas y asignarlas a los técnicos que las realizarán. Para ello se le solicita una aplicación que permita:
+                    o	A) El ingreso de una cantidad de operadores (identificados por un número de operador).
+                    o	B) El ingreso de una cantidad de órdenes de trabajo (identificadas por un número de órden).
+                    o	C) La asignación de una orden a un operador. Para ello, el usuario indicará un número de operador y el sistema le asignará la próxima orden de trabajo no asignada, teniendo en cuenta el orden de carga del punto A), dando por terminada la asignación anterior en caso de existir una. Este proceso se repetirá tantas veces como indique el usuario.
+                    o	D) Al terminar, reporte: cuántas órdenes cumplió cada operador, qué órdenes quedaron pendientes de asignar.
+
+            */
+
             Console.WriteLine("Hello World!");
 
         }
